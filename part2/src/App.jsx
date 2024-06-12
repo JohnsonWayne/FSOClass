708b348ba9876bc71b15cd17fed11edd75d3f331
Wayne Johnson <wayneajohnsonjr@gmail.com>
2024-06-12 12:32:55 -0400

    Part2c

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,10 +1,25 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+
+import axios from 'axios'
 import Note from './components/Note'
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
-  const [newNote, setNewNote] = useState(' a new note....')
+const App = () => {
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+
+  const hook = () => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
+  }
+  
+  useEffect(hook, [])
+  console.log('render', notes.length, 'notes')
 
   const addNote = (event) => {
     event.preventDefault()
@@ -41,7 +56,8 @@ const App = (props) => {
         )}
       </ul>
       <form onSubmit={addNote}>
-        <input 
+        <input
+          placeholder='a new note...'
           value={newNote}
           onChange={handleNoteChange}
         />
