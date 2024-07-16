f71e568e07740215bc3f9b8f267a1a91d082930b
Wayne Johnson <wayneajohnsonjr@gmail.com>
2024-07-16 14:01:17 -0400

    Updated

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -4,12 +4,10 @@ const app = express()
 const morgan = require('morgan')
   
 app.use(express.json())
-
-morgan.token('type', function (req, res) {return JSON.stringify(req.body)})
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :type'))
-
 app.use(express.static('dist'))
 app.use(cors())
+morgan.token('type', function (req, res) {return JSON.stringify(req.body)})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :type'))
 
 let persons = [
     { 
